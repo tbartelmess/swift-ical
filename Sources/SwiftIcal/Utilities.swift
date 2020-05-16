77e0b1aa17c4fd1a5b6a60c0c2cd129787697dd9
Thomas Bartelmess <tbartelmess@marketcircle.com>
2020-05-15 21:53:56 -0400

    Use a fixed timezone

--- Sources/SwiftIcal/Utilities.swift
+++ Sources/SwiftIcal/Utilities.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
+public let SantiagoTimezone = TimeZone(identifier: "America/Santiago")!
+
 extension Date {
     /// Convenience to return date components.
     ///
     /// - parameter timezone: `TimeZone` used to generate the date components. By default the current timezone is used.
     /// - parameter calendar: `Calendar` used to generate the data components. By default the gregorian calendar is used. Currently SwiftIcal can only supports Gregorian calendars.
-    public func components(in timezone: TimeZone = .current,
+    public func components(in timezone: TimeZone = SantiagoTimezone,
                            calendar: Calendar = Calendar(identifier: .gregorian)) -> DateComponents {
         var components = calendar.dateComponents(in: timezone, from: self)
         components.calendar = calendar
